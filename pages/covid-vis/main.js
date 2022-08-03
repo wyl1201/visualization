c40db1bbb92485c028eaa7fa403ad050853c1fff
alvin.wang <alvin.wang@wayz.ai>
2022-08-03 18:13:05 +0800

    feat(covid): 绘制世界地图

--- pages/covid-vis/main.js
+++ pages/covid-vis/main.js
@@ -1,1 +1,50 @@
-import CovidData from './asset/covid-data.json'
+import '/style.css'
+import './main.css'
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+
+const {width, height} = canvas.getBoundingClientRect()
+
+function projection([longitude, latitude]) {
+    const x = width * (180 + longitude) / 360
+    const y = height * (1- (90 + latitude) / 180)
+    return [x, y]
+}
+
+
+function drawPoints(ctx, points) {
+    ctx.beginPath();
+    ctx.moveTo(...points[0]);
+    for(let i = 1; i < points.length; i++) {
+      ctx.lineTo(...points[i]);
+    }
+    ctx.fill();
+  }
+
+(async function() {
+    const worldData = await(await (fetch('./asset/world-geojson.json'))).json()
+    console.log(`🚀 ~ file: main.js ~ line 14 ~ worldData`, worldData)
+
+    const features = worldData.features;
+      features.forEach(({geometry}) => {
+        if(geometry.type === 'MultiPolygon') {
+          const coordinates = geometry.coordinates;
+          if(coordinates) {
+            coordinates.forEach((contours) => {
+              contours.forEach((path) => {
+                const points = path.map(projection);
+                drawPoints(ctx, points);
+              });
+            });
+          }
+        } else if(geometry.type === 'Polygon') {
+          const contours = geometry.coordinates;
+          contours.forEach((path) => {
+            const points = path.map(projection);
+            drawPoints(ctx, points);
+          });
+        }
+      });
+})()
